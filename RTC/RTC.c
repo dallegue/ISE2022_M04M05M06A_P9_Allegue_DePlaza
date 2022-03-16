49310652b39280268dad94b73e1abe928c48582f
dallegue <d.allegue@alumnos.upm.es>
2022-03-16 21:00:06 +0100

    Apartado 1 (P2_1) completo.
    Add funcionalidad de parpadeo de led durante 5 segundos cada minuto.

--- RTC/RTC.c
+++ RTC/RTC.c
@@ -1,54 +1,88 @@
-/*------------------------------------------------------------------------------
- * MDK Middleware - Component ::Network
- * Copyright (c) 2004-2014 ARM Germany GmbH. All rights reserved.
- *------------------------------------------------------------------------------
- * Name:  HTTP_Server.c
- * Purpose: HTTP Server example
- *----------------------------------------------------------------------------*/
+/* Includes ------------------------------------------------------------------*/
 
 #include <stdio.h>
 #include <stdint.h>
 #include "lpc17xx_rtc.h"
 #include "lcd.h"
 #include "cmsis_os.h"                   /* CMSIS RTOS definitions             */
+#include "GPIO_LPC17xx.h"
+#include <stdbool.h>
+
+/* Macros --------------------------------------------------------------------*/
+
+#define PORT_LEDS 1
+#define PIN_LED4 23
+#define LED_ON 1
+#define LED_OFF 0
+#define DURACION_PARPADEO_MS 5000
+#define PERIODO_REFRESCO_MS 500
+
+/* Public variables ----------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+static uint16_t cuenta = 0;
+static uint8_t estado_led = LED_OFF;
+static bool parpadear_led = false;
+
+/* Function prototypes -------------------------------------------------------*/
 
 void RTC_IRQHandler(void);
+static void RTC_startup(void);
 
 
-/*----------------- INTERRUPT SERVICE ROUTINES --------------------------*/
-/*********************************************************************//**
- * @brief    RTC interrupt handler sub-routine
- * @param[in]  None
- * @return     None
- **********************************************************************/
+/* Public functions --------------------------------------------------------- */
+
+/* ISR */
 void RTC_IRQHandler(void)
 {
-  uint32_t secval;
-
   /* This is increment counter interrupt*/
   if (RTC_GetIntPending(LPC_RTC, RTC_INT_COUNTER_INCREASE))
   {
-    secval = RTC_GetTime (LPC_RTC, RTC_TIMETYPE_SECOND);
-
+    parpadear_led = true;
+    
     // Clear pending interrupt
     RTC_ClearIntPending(LPC_RTC, RTC_INT_COUNTER_INCREASE);
   }
+}
 
-  /* Continue to check the Alarm match*/
-  if (RTC_GetIntPending(LPC_RTC, RTC_INT_ALARM))
+int main (void)
+{
+  RTC_TIME_Type RTCFullTime;
+  char lineaHora[20];
+  char lineaFecha[20];
+  
+  GPIO_SetDir (PORT_LEDS, PIN_LED4, GPIO_DIR_OUTPUT); 
+  RTC_startup();
+  LCD_SPI_startup();
+  
+  /* Loop forever */
+  while(1)
   {
-
-    // Clear pending interrupt
-    RTC_ClearIntPending(LPC_RTC, RTC_INT_ALARM);
+    // Get and print current time
+    RTC_GetFullTime (LPC_RTC, &RTCFullTime);
+    sprintf(lineaHora, "%02d:%02d:%02d", RTCFullTime.HOUR, RTCFullTime.MIN, RTCFullTime.SEC);
+    sprintf(lineaFecha, "%02d/%02d/%02d", RTCFullTime.DOM, RTCFullTime.MONTH, RTCFullTime.YEAR);
+    print_lineas(lineaHora, lineaFecha);
+    osDelay(PERIODO_REFRESCO_MS);
+    
+    if (parpadear_led)
+    {
+      estado_led = (estado_led == LED_OFF) ? LED_ON : LED_OFF;
+      GPIO_PinWrite (PORT_LEDS, PIN_LED4, estado_led);
+      
+      if (++cuenta == DURACION_PARPADEO_MS/PERIODO_REFRESCO_MS)
+      {
+        cuenta = 0;
+        parpadear_led = false;
+      }
+    }
+    
+    osThreadYield ();
   }
 }
 
-/*-------------------------MAIN FUNCTION------------------------------*/
-/*********************************************************************//**
- * @brief    c_entry: Main RTC program body
- * @param[in]  None
- * @return     int
- **********************************************************************/
+/* Private functions -------------------------------------------------------- */
+
 static void RTC_startup(void)
 {
   /* RTC Block section ------------------------------------------------------ */
@@ -76,30 +110,8 @@ static void RTC_startup(void)
   RTC_SetAlarmTime (LPC_RTC, RTC_TIMETYPE_SECOND, 10);
 
   /* Set the CIIR for second counter interrupt -> cambiar a cada minuto*/
-  //RTC_CntIncrIntConfig (LPC_RTC, RTC_TIMETYPE_SECOND, ENABLE);
+  RTC_CntIncrIntConfig (LPC_RTC, RTC_TIMETYPE_MINUTE, ENABLE);
 
   /* Enable RTC interrupt */
   NVIC_EnableIRQ(RTC_IRQn);
-}
-
-int main (void)
-{
-  RTC_TIME_Type RTCFullTime;
-  char lineaHora[20];
-  char lineaFecha[20];
-  
-  RTC_startup();
-  LCD_SPI_startup();
-  
-  /* Loop forever */
-  while(1)
-  {
-    // Get and print current time
-    RTC_GetFullTime (LPC_RTC, &RTCFullTime);
-    sprintf(lineaHora, "%02d:%02d:%02d", RTCFullTime.HOUR, RTCFullTime.MIN, RTCFullTime.SEC);
-    sprintf(lineaFecha, "%02d/%02d/%02d", RTCFullTime.DOM, RTCFullTime.MONTH, RTCFullTime.YEAR);
-    print_lineas(lineaHora, lineaFecha);
-    osDelay(100);
-    osThreadYield ();
-  }
 }
